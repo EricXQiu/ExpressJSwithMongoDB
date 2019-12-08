ef29eedb8a6318f51bb03f15723223c1ac4d8bc2
Eric (Xiaoxing) <40647952+JacePhoenix@users.noreply.github.com>
2019-12-08 00:08:18 -0600

    update with mongoose

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users', {
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 3,
+        trim: true
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 3,
+        trim: true
+    },
+
+    isActive: {
+        type: Number,
+        default: 0,
+
+    }
+});
+
+module.exports = {User};
