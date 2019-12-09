09c2783219b4e1cc6da3e348cd3b5aec4083863f
Xiaoxing Qiu <xiaoxingqiu@Xiaoxings-MacBook-Pro.local>
2019-12-08 22:57:01 -0600

    update

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
-
 const User = require('./models/User');
 
-mongoose.connect('mongodb://localhost:27017/mongoose');
-mongoose.connection
-    .once('open', () => console.log('DB is CONNECTED!'))
-    .on('error', error => {
-        console.log('Could not connect to MongoDB', error);
-    });
+const express = require('express');
 
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('ROOT');
+});
+
+app.post('/users', (request, response) => {});
+
+// mongoose.connect('mongodb://localhost:27017/mongoose');
+// mongoose.connection
+//     .once('open', () => console.log('DB is CONNECTED!'))
+//     .on('error', error => {
+//         console.log('Could not connect to MongoDB', error);
+//     });
+//
 const newUser = new User({
     firstName: 'Eric',
     lastName: 'Phoenix',
@@ -20,4 +29,9 @@ newUser.save(function (error, dataSaved) {
     if (error) return console.log(error.message);
 
     console.log(dataSaved);
+});
+
+const port = 8800 || process.env.PORT;
+app.listen(port, () => {
+    console.log(`The app is listening on port ${port}!`);
 });
